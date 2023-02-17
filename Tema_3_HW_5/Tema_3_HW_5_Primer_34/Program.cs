065d9d111f34b0b87fbd66c27614c876633a39dd
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-17 04:42:37 +0300

    Домашка

---
+++ Tema_3_HW_5/Tema_3_HW_5_Primer_34/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+/*
+1. Инициализируем массив
+2. Заполняем массив случайными числами
+3. Считаем количество четных элементов в массиве
+4. Выводим на экран
+*/
+
+int[] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+int EvenCount(int[] arr)
+{
+    var count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int c = arr[i] % 2;
+        if (c == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine();
+int[] array = InitArray(15);
+int count = EvenCount(array);
+
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine($"-> В указанном массиве находтся {count} четных чисел.");
+
---
+++ Tema_3_HW_5/Tema_3_HW_5_Primer_36/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+
+*/
+
+/*
+1. Инициализируем массив
+2. Заполняем массив случайными числами
+3. Считаем сумму элементов, стоящих на нечётных позициях
+4. Выводим на экран
+*/
+
+int [] InitArray ( int dimension)
+{
+int [] arr = new int [dimension];
+Random rnd = new Random();
+for (int i=0; i<dimension; i++)
+{
+arr[i] = rnd.Next(-99,100);
+}
+return arr;
+}
+
+int SumEvenPosizion (int[] arr)
+{
+    int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+    { int c = i % 2;
+       if (c == 1)
+       sum += arr[i];
+        }
+        return sum;
+}
+Console.WriteLine();
+int[] array = InitArray(15);
+int sum = SumEvenPosizion (array);
+
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine($"-> Cуммa элементов {sum}, стоящих на нечётных позициях");
